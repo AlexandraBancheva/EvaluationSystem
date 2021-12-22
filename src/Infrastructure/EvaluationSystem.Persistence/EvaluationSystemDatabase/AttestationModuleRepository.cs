fec6e1f8f5f64808c9d9b29ab65c3523a25d42fc
VSG\Alexandra <ABancheva@vsgbg.com>
2021-12-22 17:07:54 +0200

    Added attestation module repository

---
+++ src/Infrastructure/EvaluationSystem.Persistence/EvaluationSystemDatabase/AttestationModuleRepository.cs
@@ -0,0 +1,14 @@
+﻿using EvaluationSystem.Application.Repositories;
+using EvaluationSystem.Domain.Entities;
+using EvaluationSystem.Persistence.QuestionDatabase;
+
+namespace EvaluationSystem.Persistence.EvaluationSystemDatabase
+{
+    public class AttestationModuleRepository : BaseRepository<AttestationModule>, IAttestationModuleRepository
+    {
+        public AttestationModuleRepository(IUnitOfWork unitOfWork) 
+            : base(unitOfWork)
+        {
+        }
+    }
+}
