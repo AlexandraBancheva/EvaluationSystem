24f3632de783f4ca5772408ab63366f3d9cea051
VSG\Alexandra <ABancheva@vsgbg.com>
2021-12-22 17:03:09 +0200

    Added attestation answer repo and attestation answer service

---
+++ src/Core/EvaluationSystem.Application/Repositories/IAttestationAnswerRepository.cs
@@ -0,0 +1,8 @@
+﻿using EvaluationSystem.Domain.Entities;
+
+namespace EvaluationSystem.Application.Repositories
+{
+    public interface IAttestationAnswerRepository : IRepository<AttestationAnswer>
+    {
+    }
+}
---
+++ src/Infrastructure/EvaluationSystem.Persistence/EvaluationSystemDatabase/AttestationAnswerRepository.cs
@@ -0,0 +1,14 @@
+﻿using EvaluationSystem.Application.Repositories;
+using EvaluationSystem.Domain.Entities;
+using EvaluationSystem.Persistence.QuestionDatabase;
+
+namespace EvaluationSystem.Persistence.EvaluationSystemDatabase
+{
+    public class AttestationAnswerRepository : BaseRepository<AttestationAnswer>, IAttestationAnswerRepository
+    {
+        public AttestationAnswerRepository(IUnitOfWork unitOfWork) 
+            : base(unitOfWork)
+        {
+        }
+    }
+}
